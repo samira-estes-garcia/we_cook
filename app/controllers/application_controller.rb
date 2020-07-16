574398a43ef9c2cea3dcb71cfad94c0058274f25
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-07-16 14:13:24 -0500

    added back verify_user, added views to user and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    #before_action :verified_user
+    before_action :verified_user
     helper_method :current_user
 
   private
 
-#   def verified_user
-#     redirect_to '/' unless user_is_authenticated
-#   end
+  def verified_user
+    redirect_to '/' unless user_is_authenticated
+  end
 
   def user_is_authenticated
     !!current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    #skip_before_action :verified_user, only: [:new, :create]
+    skip_before_action :verified_user, only: [:new, :create]
 
     # Log user in
     def new
