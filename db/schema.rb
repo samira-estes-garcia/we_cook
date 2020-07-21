41534026c5f52bdba3e4c0a935c01f1f9697c9a6
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-07-21 15:09:05 -0500

    added image attributes to recipe model, added image to recipes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_212258) do
+ActiveRecord::Schema.define(version: 2020_07_21_200650) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_07_16_212258) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
