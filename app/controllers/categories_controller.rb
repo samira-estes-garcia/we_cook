bdd99171290a46d61d3b01664cac8c272fd88ac6
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-08-02 17:31:15 -0500

    completely styled all pages, added @recipe to categories controller, changed images on recipes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,5 +11,6 @@ class CategoriesController < ApplicationController
     def show
     @category = Category.find(params[:id])
     @recipes = @category.recipes.order("created_at DESC")
+    @recipe = @category.recipes
     end
 end
