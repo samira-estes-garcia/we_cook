c2c499c9542eb97ccde62820d5cc7cc609d3ac3e
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-08-07 10:50:05 -0500

    styled search bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,9 @@
 class CategoriesController < ApplicationController
-    def index
-     @breakfast_categories = Category.all.where(:name => 'breakfast')
-     @lunch_categories = Category.all.where(:name => 'lunch')
-     @dinner_categories = Category.all.where(:name => 'dinner')
-     @dessert_categories = Category.all.where(:name => 'dessert')
-     @snack_categories = Category.all.where(:name => 'snack')
-     @beverage_categories = Category.all.where(:name => 'beverage')
-    end
 
     def show
     @category = Category.find(params[:id])
     @recipes = @category.recipes.order("created_at DESC")
     @recipe = @category.recipes
     end
+
 end
